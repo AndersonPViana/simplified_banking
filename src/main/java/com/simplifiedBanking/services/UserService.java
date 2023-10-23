7babec629824523d9f5994640dc75ed277a7d7ee
Anderson Viana <andersonp.viana.contato@gmail.com>
2023-10-23 14:09:46 -0300

    feat: create User Service

---
+++ src/main/java/com/simplifiedBanking/services/UserService.java
@@ -0,0 +1,47 @@
+package com.simplifiedBanking.services;
+
+import com.simplifiedBanking.domain.user.User;
+import com.simplifiedBanking.domain.user.UserType;
+import com.simplifiedBanking.dtos.UserDTO;
+import com.simplifiedBanking.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if(sender.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Usuário do tipo lojista não está autorizado a realizar transação");
+        }
+
+        if(sender.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Saldo insuficiente");
+        }
+    }
+
+    public User createUser(UserDTO userDTO) {
+        User newUser = new User(userDTO);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+
+    public void saveUser(User user) {
+        this.repository.save(user);
+    }
+
+    public List<User> findAll() {
+        return this.repository.findAll();
+    }
+
+
+}
