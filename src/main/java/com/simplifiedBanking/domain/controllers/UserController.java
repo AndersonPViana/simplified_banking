a9af00fbb2a1e4f06e7aad10de229eb118d92e45
Anderson Viana <andersonp.viana.contato@gmail.com>
2023-10-23 14:10:24 -0300

    feat: create User Controller

---
+++ src/main/java/com/simplifiedBanking/domain/controllers/UserController.java
@@ -0,0 +1,30 @@
+package com.simplifiedBanking.domain.controllers;
+
+import com.simplifiedBanking.domain.user.User;
+import com.simplifiedBanking.dtos.UserDTO;
+import com.simplifiedBanking.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody UserDTO userDTO) {
+        User newUser = service.createUser(userDTO);
+        return new ResponseEntity<>(newUser, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = this.service.findAll();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+}
